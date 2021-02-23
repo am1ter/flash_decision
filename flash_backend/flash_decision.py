b77592178b2b8dee07248e93c7501399a8bb600b
Alexey Semyonov <amiter@yandex.ru>
2021-02-23 22:26:33 +0300

    Flash decision v.0.3.01 Application split into 2 independent microservices: backend (python) + frontend (vuejs via nodejs)

--- flash_backend/flash_decision.py
+++ flash_backend/flash_decision.py
@@ -5,6 +5,6 @@ if __name__ == '__main__':
     from app import app
 
     # Run production server using waitress
-    serve(app, host='0.0.0.0', port=8080)
+    serve(app, host='0.0.0.0', port=8001)
 
 
