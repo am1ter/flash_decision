4181dbb691a895b59c641e7455ab0890163f83f9
Aleksei Semenov <amiter@yandex.ru>
2023-06-06 15:17:43 -0300

    FD18: Add unit tests for JWT
    Add 4 unit tests
    Rename file with service `Authorization`

--- backend/tests/unit/services/test_unit_service_user.py
+++ backend/tests/unit/services/test_unit_service_user.py
@@ -3,14 +3,20 @@ from datetime import datetime
 from random import randint
 from unittest import IsolatedAsyncioTestCase
 
+from jose import jwt
+
 from app.api.schemas.user import ReqSignIn, ReqSignUp, ReqSystemInfo
 from app.domain.user import DomainAuth, DomainUser
 from app.infrastructure.repositories.base import Repository
 from app.services.user import ServiceUser
+from app.services.user_authorization import ServiceAuthorization
+from app.system.config import settings
 from app.system.constants import UserStatus
 from app.system.exceptions import (
     EmailValidationError,
+    InvalidJwtError,
     IpAddressValidationError,
+    JwtExpiredError,
     UserDisabledError,
     WrongPasswordError,
 )
@@ -115,3 +121,45 @@ class TestServiceUser(IsolatedAsyncioTestCase):
         req_system_info_wrong_ip.ip_address = "192.168.0."
         with self.assertRaises(IpAddressValidationError):
             await self.service.sign_up(self.req_sign_up, req_system_info_wrong_ip)
+
+    async def test_create_access_token(self) -> None:
+        user = await self.service.sign_up(self.req_sign_up, self.req_system_info)
+        token_encoded = self.service.create_access_token(user)
+        token_decoded = jwt.decode(token=token_encoded.access_token, key=settings.JWT_SECRET_KEY)
+        self.assertEqual(token_decoded["sub"], user.email.value)
+        self.assertGreater(token_decoded["exp"], token_decoded["iat"])
+
+
+class TestServiceAuthorization(IsolatedAsyncioTestCase):
+    async def asyncSetUp(self) -> None:
+        repository = RepositoryUserFake()
+        self.service_user = ServiceUser(repository=repository)  # type: ignore[arg-type]
+        req_sign_up = ReqSignUp(
+            email="test-signup@alekseisemenov.ru",
+            name="test-signup",
+            password="uc8a&Q!W",  # noqa: S106
+        )
+        req_system_info = ReqSystemInfo(ip_address="127.0.0.1", user_agent="Test")
+        self.user_sign_up = await self.service_user.sign_up(req_sign_up, req_system_info)
+        self.token_encoded = self.service_user.create_access_token(self.user_sign_up)
+
+    async def test_authorization_success(self) -> None:
+        self.service_auth = ServiceAuthorization(self.token_encoded.access_token, self.service_user)
+        user_by_token = await self.service_auth.get_current_user()
+        self.assertEqual(user_by_token, self.user_sign_up)
+
+    async def test_authorization_failure_invalid(self) -> None:
+        token_encoded_invalid = self.token_encoded.access_token + "wrong"
+        with self.assertRaises(InvalidJwtError):
+            self.service_auth = ServiceAuthorization(token_encoded_invalid, self.service_user)
+
+    async def test_authorization_failure_expired(self) -> None:
+        token_encoded_valid = self.token_encoded.access_token
+        token_decoded_valid = jwt.decode(token=token_encoded_valid, key=settings.JWT_SECRET_KEY)
+        token_decoded_invalid = copy(token_decoded_valid)
+        token_decoded_invalid["exp"] = token_decoded_invalid["iat"] - 100
+        token_encoded_invalid = jwt.encode(
+            token_decoded_invalid, settings.JWT_SECRET_KEY, settings.JWT_ALGORITHM
+        )
+        with self.assertRaises(JwtExpiredError):
+            self.service_auth = ServiceAuthorization(token_encoded_invalid, self.service_user)
