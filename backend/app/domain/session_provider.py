6412c3b05011d7ae9a1e2325e74a3d811938863c
Aleksei Semenov <amiter@yandex.ru>
2023-06-27 11:43:10 -0300

    FD19: Reformat data frames received from `AlphaVantage` data proivder
    Add methods for data frames transformations to `ProviderAlphaVantage`
    Use `pyarrow` as pandas engine
    Update pandas

--- backend/app/domain/session_provider.py
+++ backend/app/domain/session_provider.py
@@ -52,6 +52,7 @@ class ProviderAlphaVantage:
     url_root = "https://www.alphavantage.co"
     api_key = settings.ALPHAVANTAGE_API_KEY
     url_get_list: str
+    data_cols_final = ["datetime", "open", "high", "low", "close", "volume"]
 
     def _download_csv(self) -> csv_table:
         r = requests.get(self.url_get_list)
@@ -68,6 +69,26 @@ class ProviderAlphaVantage:
     async def _download_data(self, ticker: Ticker, timeframe: Timeframe) -> pd.DataFrame:
         raise NotImplementedError
 
+    @classmethod
+    def _transform_df_quotes(cls, df_quotes: pd.DataFrame) -> pd.DataFrame:
+        # Validate df is not empty
+        if df_quotes.empty:
+            raise ProviderInvalidDataError
+
+        # Use pyarrow engine to work with pandas
+        df_quotes = pd.DataFrame.convert_dtypes(df_quotes, dtype_backend="pyarrow")
+
+        # Drop extra columns
+        cols_to_del = [col for col in df_quotes.columns if col not in cls.data_cols_final]
+        df_quotes = df_quotes.drop(cols_to_del, axis=1)
+        if list(df_quotes.columns) != cls.data_cols_final:
+            raise ProviderInvalidDataError
+
+        # Convert column from string to datetime
+        df_quotes["datetime"] = pd.to_datetime(df_quotes["datetime"])
+
+        return df_quotes
+
     def get_list(self, force: bool | None = None) -> list[Ticker]:
         if not self.all_tickers or force:
             # Do not crash the app if the list of tickers is not available
@@ -82,15 +103,33 @@ class ProviderAlphaVantage:
 
     async def get_data(self, ticker: Ticker, timeframe: Timeframe) -> pd.DataFrame:
         data = await self._download_data(ticker, timeframe)
-        if data.empty:
-            raise ProviderInvalidDataError
+        data = self._transform_df_quotes(data)
         return data
 
 
 class ProviderAlphaVantageStocks(ProviderAlphaVantage):
+    data_cols_intraday = {
+        "index": "datetime",
+        "1. open": "open",
+        "2. high": "high",
+        "3. low": "low",
+        "4. close": "close",
+        "5. volume": "volume",
+    }
+    data_cols_daily = {
+        "index": "datetime",
+        "1. open": "open",
+        "2. high": "high",
+        "3. low": "low",
+        "4. close": "close",
+        "6. volume": "volume",
+    }
+
     def __init__(self) -> None:
         self.url_get_list = f"{self.url_root}/query?function=LISTING_STATUS&apikey={self.api_key}"
-        self.exporter = TimeSeries(key=settings.ALPHAVANTAGE_API_KEY, output_format="pandas")
+        self.exporter = TimeSeries(
+            key=settings.ALPHAVANTAGE_API_KEY, output_format="pandas", indexing_type="integer"
+        )
 
     def _process_csv(self, csv_table: csv_table) -> list[Ticker]:
         all_tickers = []
@@ -115,19 +154,32 @@ class ProviderAlphaVantageStocks(ProviderAlphaVantage):
         return all_tickers
 
     async def _download_data(self, ticker: Ticker, timeframe: Timeframe) -> pd.DataFrame:
-        """Send request, use data with quotes, ignore metadata (_)"""
+        """Send request, use data with quotes, ignore metadata (_), rename columns"""
         match timeframe:
             case Timeframe.daily:
                 data, _ = await self.exporter.get_daily_adjusted(ticker.symbol)
+                data = data.rename(self.data_cols_daily, axis="columns")
             case _:
                 data, _ = await self.exporter.get_intraday(ticker.symbol, interval=timeframe.value)
+                data = data.rename(self.data_cols_intraday, axis="columns")
         return data
 
 
 class ProviderAlphaVantageCrypto(ProviderAlphaVantage):
+    related_columns_daily = {
+        "index": "datetime",
+        "1a. open (USD)": "open",
+        "2b. high (USD)": "high",
+        "3b. low (USD)": "low",
+        "4b. close (USD)": "close",
+        "5. volume": "volume",
+    }
+
     def __init__(self) -> None:
         self.url_get_list = f"{self.url_root}/digital_currency_list/"
-        self.exporter = CryptoCurrencies(key=settings.ALPHAVANTAGE_API_KEY, output_format="pandas")
+        self.exporter = CryptoCurrencies(
+            key=settings.ALPHAVANTAGE_API_KEY, output_format="pandas", indexing_type="integer"
+        )
 
     def _process_csv(self, csv_table: csv_table) -> list[Ticker]:
         all_tickers = []
@@ -151,12 +203,13 @@ class ProviderAlphaVantageCrypto(ProviderAlphaVantage):
         return all_tickers
 
     async def _download_data(self, ticker: Ticker, timeframe: Timeframe) -> pd.DataFrame:
-        """Send request, use data with quotes, ignore metadata (_)"""
+        """Send request, use data with quotes, ignore metadata (_), rename columns"""
         match timeframe:
             case Timeframe.daily:
                 data, _ = await self.exporter.get_digital_currency_daily(
                     ticker.symbol, market=settings.CRYPTO_PRICE_CURRENCY
                 )
+                data = data.rename(self.related_columns_daily, axis="columns")
             case _:
                 raise UnsupportedModeError
         return data
