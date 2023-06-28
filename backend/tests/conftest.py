aa8e0096fe3e2f12a2f9466a9b2cfc0fee55db43
Aleksei Semenov <amiter@yandex.ru>
2023-06-28 13:01:51 -0300

    FD19: Optimize data providers' unit tests
    Use inheritance with method override instead of monkey patching
    Move fixtures-like entities to conftest to reuse them later

--- backend/tests/conftest.py
+++ backend/tests/conftest.py
@@ -3,6 +3,11 @@ import uuid
 import pytest
 
 from app.api.schemas.user import ReqSignIn, ReqSignUp
+from app.domain.session_provider import (
+    ProviderAlphaVantageCrypto,
+    ProviderAlphaVantageStocks,
+    csv_table,
+)
 from app.domain.user import DomainUser
 from app.system.constants import UserStatus
 
@@ -29,3 +34,40 @@ def req_sign_up() -> ReqSignUp:
 @pytest.fixture(scope="module")
 def req_sign_in(req_sign_up: ReqSignUp) -> ReqSignIn:
     return ReqSignIn(username=req_sign_up.email, password=req_sign_up.password)
+
+
+class ProviderAVStocksMockSuccess(ProviderAlphaVantageStocks):
+    def _download_csv(self) -> csv_table:
+        return [
+            ["MSFT", "Microsoft Corp", "NASDAQ", "Stock", "13.03.1986", "null", "Active"],
+            ["AAPL", "Apple Inc", "NASDAQ", "Stock", "12.12.1980", "null", "Active"],
+            ["BA", "Boeing Company", "NYSE", "Stock", "02.01.1962", "null", "Active"],
+        ]
+
+
+class ProviderAVCryptoMockSuccess(ProviderAlphaVantageCrypto):
+    def _download_csv(self) -> csv_table:
+        return [["BTC", "Bitcoin"], ["ETH", "Ethereum"]]
+
+
+class ProviderAVStocksMockFailureBrokenTicker(ProviderAlphaVantageStocks):
+    def _download_csv(self) -> csv_table:
+        return [
+            ["", "", "NASDAQ", "Stock", "13.03.1986", "null", "Active"],
+            ["AAPL", "Apple Inc", "NASDAQ", "Stock", "12.12.1980", "null", "Active"],
+        ]
+
+
+class ProviderAVCryptoMockFailureBrokenTicker(ProviderAlphaVantageCrypto):
+    def _download_csv(self) -> csv_table:
+        return [["", "Bitcoin"], ["ETH", "Ethereum"]]
+
+
+class ProviderAVStocksMockFailureBlank(ProviderAlphaVantageStocks):
+    def _download_csv(self) -> csv_table:
+        return []
+
+
+class ProviderAlVCryptoMockFailureBlank(ProviderAlphaVantageCrypto):
+    def _download_csv(self) -> csv_table:
+        return []
--- backend/tests/unit/domain/test_unit_domain_session_provider.py
+++ backend/tests/unit/domain/test_unit_domain_session_provider.py
@@ -1,92 +1,46 @@
-from collections.abc import Callable
-
 import pytest
 import structlog
 
-from app.domain.session_provider import (
-    ProviderAlphaVantage,
-    ProviderAlphaVantageCrypto,
-    ProviderAlphaVantageStocks,
-    Ticker,
-    csv_table,
-)
+from app.domain.session_provider import Provider, Ticker
 from app.system.exceptions import ProviderAccessError, ProviderInvalidDataError
-
-prov_av_stocks = ProviderAlphaVantageStocks
-prov_av_crypto = ProviderAlphaVantageCrypto
-
-
-def download_csv_mock_stocks(*args) -> csv_table:
-    return [
-        ["MSFT", "Microsoft Corp", "NASDAQ", "Stock", "13.03.1986", "null", "Active"],
-        ["AAPL", "Apple Inc", "NASDAQ", "Stock", "12.12.1980", "null", "Active"],
-        ["BA", "Boeing Company", "NYSE", "Stock", "02.01.1962", "null", "Active"],
-    ]
-
-
-def download_csv_mock_broken_ticker_stocks(*args) -> csv_table:
-    return [
-        ["", "", "NASDAQ", "Stock", "13.03.1986", "null", "Active"],
-        ["AAPL", "Apple Inc", "NASDAQ", "Stock", "12.12.1980", "null", "Active"],
-    ]
-
-
-def download_csv_mock_crypto(*args) -> csv_table:
-    return [["BTC", "Bitcoin"], ["ETH", "Ethereum"]]
-
-
-def download_csv_mock_broken_ticker_crypto(*args) -> csv_table:
-    return [["", "Bitcoin"], ["ETH", "Ethereum"]]
+from tests.conftest import (
+    ProviderAlVCryptoMockFailureBlank,
+    ProviderAVCryptoMockFailureBrokenTicker,
+    ProviderAVCryptoMockSuccess,
+    ProviderAVStocksMockFailureBlank,
+    ProviderAVStocksMockFailureBrokenTicker,
+    ProviderAVStocksMockSuccess,
+)
 
 
 class TestProviderAlphaVantage:
-    def _download_csv_mock_blank(self) -> csv_table:
-        return []
-
     @pytest.mark.parametrize(
-        ("provider_class", "download_csv_mock"),
-        [(prov_av_stocks, download_csv_mock_stocks), (prov_av_crypto, download_csv_mock_crypto)],
+        "provider_cls",
+        [ProviderAVStocksMockSuccess, ProviderAVCryptoMockSuccess],
     )
-    def test_get_list_success(
-        self,
-        provider_class: type[ProviderAlphaVantage],
-        download_csv_mock: Callable[[], csv_table],
-        monkeypatch: pytest.MonkeyPatch,
-    ) -> None:
-        monkeypatch.setattr(provider_class, "_download_csv", download_csv_mock)
-        provider = provider_class()
+    def test_get_list_success(self, provider_cls: type[Provider]) -> None:
+        provider = provider_cls()
         stocks = provider.get_list()
-        assert len(stocks) == len(download_csv_mock())
+        assert len(stocks) > 0
         assert isinstance(stocks[0], Ticker)
 
-    @pytest.mark.parametrize("provider_class", [prov_av_stocks, prov_av_crypto])
-    def test_get_list_failure_blank(
-        self,
-        provider_class: type[ProviderAlphaVantage],
-        monkeypatch: pytest.MonkeyPatch,
-    ) -> None:
-        monkeypatch.setattr(provider_class, "_download_csv", self._download_csv_mock_blank)
-        provider = provider_class()
+    @pytest.mark.parametrize(
+        "provider_cls",
+        [ProviderAVStocksMockFailureBlank, ProviderAlVCryptoMockFailureBlank],
+    )
+    def test_get_list_failure_blank(self, provider_cls: type[Provider]) -> None:
+        provider = provider_cls()
         with structlog.testing.capture_logs() as logs:
             provider.get_list()
         assert len(logs) == 1
         assert logs[0]["exc_info"].msg == ProviderAccessError.msg
 
     @pytest.mark.parametrize(
-        ("provider_class", "download_csv_mock_broken_ticker"),
-        [
-            (prov_av_stocks, download_csv_mock_broken_ticker_stocks),
-            (prov_av_crypto, download_csv_mock_broken_ticker_crypto),
-        ],
+        "provider_cls",
+        [ProviderAVStocksMockFailureBrokenTicker, ProviderAVCryptoMockFailureBrokenTicker],
     )
-    def test_get_list_failure_broken_ticket(
-        self,
-        provider_class: type[ProviderAlphaVantage],
-        download_csv_mock_broken_ticker: Callable[[], csv_table],
-        monkeypatch: pytest.MonkeyPatch,
-    ) -> None:
-        monkeypatch.setattr(provider_class, "_download_csv", download_csv_mock_broken_ticker)
-        provider = provider_class()
+    def test_get_list_failure_broken_ticket(self, provider_cls: type[Provider]) -> None:
+        provider = provider_cls()
         with structlog.testing.capture_logs() as logs:
             provider.get_list()
         assert len(logs) == 1
