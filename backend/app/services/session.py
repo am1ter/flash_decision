ef72017aba29b704ca2ef024640b1105d1c7bace
Aleksei Semenov <amiter@yandex.ru>
2023-07-11 15:45:46 -0300

    FD19: Refactor `ServiceSession`
    Move some methods into independant commands

--- backend/app/services/session.py
+++ backend/app/services/session.py
@@ -49,7 +49,9 @@ class TickersColProcessed:
 
 class ServiceSession:
     """
-    Session is the key object of the application. This service determine how user interacts with it.
+    Session is the key object of the application.
+    This service determine how user interacts with it.
+    All interactions are decomposed into commands that called from this service.
     """
 
     def __init__(self, uow: UowSessionDep) -> None:
@@ -57,38 +59,68 @@ class ServiceSession:
         self.cache = Bootstrap().cache if settings.CACHE_ENABLED else None
         self.provider_stocks = Bootstrap().provider_stocks
         self.provider_crypto = Bootstrap().provider_crypto
-        # Cache keys names
-        self.ck_tickers_stocks = f"{self.provider_stocks.__class__.__name__}:_tickers"
-        self.ck_tickers_crypto = f"{self.provider_crypto.__class__.__name__}:_tickers"
+
+    async def collect_session_options(self) -> SessionOptions:
+        all_tickers = await CommandLoadTickers(self).execute()
+        options = SessionOptions(all_ticker=all_tickers)
+        await logger.ainfo_finish(cls=self.__class__, show_func_name=True)
+        return options
+
+    async def start_session(
+        self, mode: SessionMode, session_params: ReqSession | None, user: DomainUser
+    ) -> DomainSession:
+        await CommandValidateTickers(self).execute()
+        session = CommandCreateSession(self, mode, session_params, user).execute()
+        await CommandStartSession(self, session).execute()
+        await logger.ainfo_finish(cls=self.__class__, show_func_name=True, session=session)
+        return session
+
+
+@define
+class CommandLoadTickers:
+    service: ServiceSession
+
+    async def execute(self) -> list[Ticker]:
+        tickers_col_proc = await self._load_processed_tickers()
+        return list(tickers_col_proc.stocks.values()) + list(tickers_col_proc.crypto.values())
+
+    @staticmethod
+    def _cache_key_by_provider(provider: Provider) -> str:
+        return f"{provider.__class__.__name__}:_tickers"
 
     def _get_processed_tickers_from_memory(self) -> TickersColProcessed:
         return TickersColProcessed(
-            stocks=self.provider_stocks.get_tickers(),
-            crypto=self.provider_crypto.get_tickers(),
+            stocks=self.service.provider_stocks.get_tickers(),
+            crypto=self.service.provider_crypto.get_tickers(),
         )
 
     async def _get_raw_tickers_from_cache(self) -> TickersColRaw:
-        if not self.cache:
+        if not self.service.cache:
             raise CacheObjectNotFoundError
-        raw_tickers = await self.cache.mget([self.ck_tickers_stocks, self.ck_tickers_crypto])
+        raw_tickers = await self.service.cache.mget(
+            [
+                self._cache_key_by_provider(self.service.provider_stocks),
+                self._cache_key_by_provider(self.service.provider_crypto),
+            ]
+        )
         assert isinstance(raw_tickers[0], list) and isinstance(raw_tickers[1], list)
         return TickersColRaw(stocks=raw_tickers[0], crypto=raw_tickers[1])
 
     async def _get_raw_tickers_from_provider(self) -> TickersColRaw:
         tickers_col_raw = TickersColRaw(
-            stocks=await self.provider_stocks.download_raw_tickers(),
-            crypto=await self.provider_crypto.download_raw_tickers(),
+            stocks=await self.service.provider_stocks.download_raw_tickers(),
+            crypto=await self.service.provider_crypto.download_raw_tickers(),
         )
-        if self.cache:
+        if self.service.cache:
             objects_to_cache = {
-                self.ck_tickers_stocks: tickers_col_raw.stocks,
-                self.ck_tickers_crypto: tickers_col_raw.crypto,
+                self._cache_key_by_provider(self.service.provider_stocks): tickers_col_raw.stocks,
+                self._cache_key_by_provider(self.service.provider_crypto): tickers_col_raw.crypto,
             }
             with contextlib.suppress(CacheConnectionError):
-                await self.cache.mset(objects_to_cache)
+                await self.service.cache.mset(objects_to_cache)
         return tickers_col_raw
 
-    async def _load_tickers(self) -> TickersColProcessed:
+    async def _load_processed_tickers(self) -> TickersColProcessed:
         with contextlib.suppress(MemoryObjectNotFoundError):
             return self._get_processed_tickers_from_memory()
 
@@ -98,68 +130,80 @@ class ServiceSession:
             tickers_col_raw = await self._get_raw_tickers_from_provider()
 
         return TickersColProcessed(
-            self.provider_stocks.process_tickers(tickers_col_raw.stocks),
-            self.provider_crypto.process_tickers(tickers_col_raw.crypto),
+            self.service.provider_stocks.process_tickers(tickers_col_raw.stocks),
+            self.service.provider_crypto.process_tickers(tickers_col_raw.crypto),
         )
 
-    async def collect_session_options(self) -> SessionOptions:
-        tickers_col_proc = await self._load_tickers()
-        all_ticker = list(tickers_col_proc.stocks.values()) + list(tickers_col_proc.crypto.values())
-        options = SessionOptions(all_ticker=all_ticker)
-        await logger.ainfo_finish(cls=self.__class__, show_func_name=True)
-        return options
+
+@define
+class CommandValidateTickers:
+    service: ServiceSession
+
+    async def execute(self) -> None:
+        try:
+            self.service.provider_stocks.get_tickers()
+            self.service.provider_crypto.get_tickers()
+        except MemoryObjectNotFoundError:
+            await CommandLoadTickers(service=self.service).execute()
+
+
+@define
+class CommandCreateSession:
+    service: ServiceSession
+    mode: SessionMode
+    session_params: ReqSession | None
+    user: DomainUser
+
+    def execute(self) -> DomainSession:
+        session = self._create_session()
+        session.bound_to_user(self.user)
+        return session
 
     def _find_provider(self, ticker_type: str) -> Provider:
         match TickerType(ticker_type):
             case TickerType.crypto:
-                provider = self.provider_crypto
+                provider = self.service.provider_crypto
             case _:
-                provider = self.provider_stocks
+                provider = self.service.provider_stocks
         return provider
 
-    def _create_session(
-        self, mode: SessionMode, session_params: ReqSession | None
-    ) -> DomainSession:
+    def _create_session(self) -> DomainSession:
         session: DomainSession
-        match mode:
+        match self.mode:
             case SessionMode.classic:
-                session = DomainSessionClassic.create(provider=self.provider_stocks)
+                session = DomainSessionClassic.create(provider=self.service.provider_stocks)
             case SessionMode.blitz:
-                session = DomainSessionBlitz.create(provider=self.provider_stocks)
+                session = DomainSessionBlitz.create(provider=self.service.provider_stocks)
             case SessionMode.crypto:
-                session = DomainSessionCrypto.create(provider=self.provider_crypto)
+                session = DomainSessionCrypto.create(provider=self.service.provider_crypto)
             case SessionMode.custom:
-                assert session_params
+                assert self.session_params
                 session = DomainSessionCustom.create(
-                    provider=self._find_provider(session_params.ticker_type),
-                    ticker_symbol=session_params.ticker_symbol,
-                    timeframe=session_params.timeframe,
-                    barsnumber=session_params.barsnumber,
-                    timelimit=session_params.timelimit,
-                    iterations=session_params.iterations,
-                    slippage=session_params.slippage,
-                    fixingbar=session_params.fixingbar,
+                    provider=self._find_provider(self.session_params.ticker_type),
+                    ticker_symbol=self.session_params.ticker_symbol,
+                    timeframe=self.session_params.timeframe,
+                    barsnumber=self.session_params.barsnumber,
+                    timelimit=self.session_params.timelimit,
+                    iterations=self.session_params.iterations,
+                    slippage=self.session_params.slippage,
+                    fixingbar=self.session_params.fixingbar,
                 )
             case _:
-                assert_never(mode)
+                assert_never(self.mode)
         return session
 
-    async def start_session(
-        self, mode: SessionMode, session_params: ReqSession | None, user: DomainUser
-    ) -> DomainSession:
-        # Validate if provider contains information about tickers
-        await self._load_tickers()
-        # Create session
-        session = self._create_session(mode, session_params)
-        # Link to user
-        session.bound_to_user(user)
-        # Start session
+
+@define
+class CommandStartSession:
+    service: ServiceSession
+    session: DomainSession
+
+    async def execute(self) -> DomainSession:
         try:
-            async with self.uow:
-                self.uow.task_group.create_task(session.start())
-                self.uow.repository.add(session)
-                self.uow.task_group.create_task(self.uow.commit())
+            async with self.service.uow:
+                self.service.uow.task_group.create_task(self.session.start())
+                self.service.uow.repository.add(self.session)
+                self.service.uow.task_group.create_task(self.service.uow.commit())
         except ExceptionGroup as eg:
             raise eg.exceptions[0] from eg
-        await logger.ainfo_finish(cls=self.__class__, show_func_name=True, session=session)
-        return session
+        return self.session
