ec641ca99f9c005c1e2ee63d49f8e9466b49d3cc
Aleksei Semenov <amiter@yandex.ru>
2023-08-02 22:57:43 -0300

    FD20: Add method to get iteration from db to `ServiceIteration`
    Add new method to `ServiceIteration`
    Add 2 unit test for `ServiceIteration`

--- backend/app/services/iteration.py
+++ backend/app/services/iteration.py
@@ -4,7 +4,7 @@ import structlog
 from fastapi import Depends
 from uuid6 import UUID
 
-from app.domain.iteration import DomainIterationCollection
+from app.domain.iteration import DomainIteration, DomainIterationCollection
 from app.domain.session import SessionQuotes
 from app.infrastructure.repositories.iteration import RepositoryNoSqlIteration
 from app.infrastructure.units_of_work.base_nosql import UnitOfWorkNoSqlMongo
@@ -35,9 +35,14 @@ class ServiceIteration:
         return iteration_collection
 
     async def render_chart(self, session_id: UUID, iteration_num: int) -> str:
-        async with self.uow:
-            self.uow.repository = cast(RepositoryNoSqlIteration, self.uow.repository)
-            iteration = self.uow.repository.get_iteration(session_id, iteration_num)
+        iteration = await self.get_iteration(session_id=session_id, iteration_num=iteration_num)
         chart = iteration.render_chart()
         await logger.ainfo_finish(cls=self.__class__, show_func_name=True, iteration=iteration)
         return chart
+
+    async def get_iteration(self, session_id: UUID, iteration_num: int) -> DomainIteration:
+        async with self.uow:
+            self.uow.repository = cast(RepositoryNoSqlIteration, self.uow.repository)
+            iteration = self.uow.repository.get_iteration(session_id, iteration_num)
+        await logger.ainfo_finish(cls=self.__class__, show_func_name=True, iteration=iteration)
+        return iteration
--- backend/tests/unit/services/test_unit_service_iteration.py
+++ backend/tests/unit/services/test_unit_service_iteration.py
@@ -1,3 +1,5 @@
+import json
+from collections import defaultdict
 from typing import Self
 
 import pytest
@@ -12,10 +14,13 @@ from app.services.iteration import ServiceIteration
 
 class RepositoryNoSqlIterationFake(Repository):
     def __init__(self) -> None:
-        self.storage: dict[UUID, DomainIteration] = {}
+        self.storage: dict[UUID, dict[int, DomainIteration]] = defaultdict(dict)
 
     def add(self, obj: DomainIteration) -> None:  # type: ignore[override]
-        self.storage[obj._id] = obj
+        self.storage[obj.session_id][obj.iteration_num] = obj
+
+    def get_iteration(self, session_id: UUID, iteration_num: int) -> DomainIteration:
+        return self.storage[session_id][iteration_num]
 
 
 class UnitOfWorkIterationFake(UnitOfWork):
@@ -29,12 +34,13 @@ class UnitOfWorkIterationFake(UnitOfWork):
         pass
 
 
-@pytest.fixture()
+@pytest.fixture(scope="class")
 def service_iteration() -> ServiceIteration:
     return ServiceIteration(UnitOfWorkIterationFake())  # type: ignore[arg-type]
 
 
 class TestServiceIteration:
+    @pytest.mark.dependency()
     @pytest.mark.asyncio()
     async def test_create_iterations(
         self, service_iteration: ServiceIteration, session_quotes: SessionQuotes
@@ -42,3 +48,19 @@ class TestServiceIteration:
         iteration_collection = await service_iteration.create_iterations(session_quotes)
         assert len(iteration_collection) == session_quotes.session.iterations.value
         assert isinstance(iteration_collection[0], DomainIteration)
+
+    @pytest.mark.dependency(depends=["TestServiceIteration::test_create_iterations"])
+    @pytest.mark.asyncio()
+    async def test_get_iteration(self, service_iteration: ServiceIteration) -> None:
+        session_id = list(service_iteration.uow.repository.storage.keys())[0]  # type: ignore[attr-defined]
+        iteration_num = 0
+        iteration = await service_iteration.get_iteration(session_id, iteration_num)
+        assert isinstance(iteration, DomainIteration)
+        assert iteration.iteration_num == iteration_num
+
+    @pytest.mark.dependency(depends=["TestServiceIteration::test_create_iterations"])
+    @pytest.mark.asyncio()
+    async def test_render_chart(self, service_iteration: ServiceIteration) -> None:
+        session_id = list(service_iteration.uow.repository.storage.keys())[0]  # type: ignore[attr-defined]
+        iteration = await service_iteration.render_chart(session_id=session_id, iteration_num=0)
+        assert "data" in json.loads(iteration)
